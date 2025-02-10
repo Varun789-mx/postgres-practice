a4b52fbf2d9665886edecd63aea78228f68b7ebf
Harsh <VARUNSH153@GMAIL.COM>
2025-02-11 01:04:33 +0530

    Update insert-data.ts
    erased

--- src/insert-data.ts
+++ src/insert-data.ts
@@ -1,13 +1,1 @@
-import { getclient } from "./utils";
 
-async function createEntries() {
-    const client = await getclient();
-    const inserttext = `INSERT INTO users (email,password) VALUES ($1,$2) RETURNING ID `;
-    const uservalues = [`john.doe@gmail.com`, `samplepassword`];
-    let response = await client.query(inserttext, uservalues);
-    const innertodotext = `INSERT INTO TODO (TITLE, DESCRIPTION, USER_ID, done) VALUES ($1, $2, $3, $4) RETURNING id`;
-    const todovalues = [`buy `, `random stuff `, response.rows[0].id, false];
-    await client.query(innertodotext, todovalues);
-    console.log(`Entries created successfully`);
-}
-createEntries();
