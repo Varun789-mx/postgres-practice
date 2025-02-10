102ec9538f7df0bce7596b62a40258ac8ca5f0e6
Harsh <VARUNSH153@GMAIL.COM>
2025-02-11 01:03:58 +0530

    Update utils.ts
    erased

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,1 @@
-import { Client } from "pg";
-
-export async function getclient() {
-    const client = new Client({ connectionString: 'postgresql://postgres:harsh@localhost:5431/postgres' });
-    try {
-        await client.connect();
-        console.log("Connection success");
-    } catch (error) {
-        console.log('Error occured' + error);
-    }
-    return client;
-}
 
