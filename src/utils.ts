1de89dbd1b7f1861eb04c74effb6955d2c0cd89e
Varun789-mx <varunsh153@gmail.com>
2025-02-11 19:02:33 +0000

    removed the connection string

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import {Client} from "pg";
 
 export async function getclient() { 
 	try {
-	const client = new Client({connectionString:`psql 'postgres://avnadmin:AVNS_kdZr6Y1rjd7Krl6ql_u@pg-a1248e6-varunsh153-47db.h.aivencloud.com:25254/defaultdb?sslmode=require'`});
+	const client = new Client({connectionString:``});
 		if(!client) { 
 		console.error("error while creating client");
 	}
