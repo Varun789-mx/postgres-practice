ef8c64400b1ada7bd3c2bc859014e79d19bb05ca
Varun789-mx <varunsh153@gmail.com>
2025-02-11 19:00:22 +0000

    added the basic file and this push is for testing only

--- src/create_table.ts
+++ src/create_table.ts
@@ -1,24 +1,44 @@
 import { getclient } from "./utils";
 
-async function createTable() {
-  const client = await getclient();
-  const createUsertable = `
-    CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-)`;
-  await client.query(createUsertable);
-  const createTodoquery = `
-    CREATE TABLE todo ( 
-    id SERIAL PRIMARY KEY,
-    TITLE TEXT NOT NULL,
-    DESCRIPTION TEXT,
-    USER_ID INTEGER REFERENCES users(id),
-    done BOOLEAN DEFAULT FALSE 
-);`
-;
-  await client.query(createTodoquery);
-  console.log("Tables created successfully");
+export async function createTable() { 
+	const client = await getclient();
+	if(!client) { 
+		console.log("Error while connecting to db");
+	}
+	client.connect();
+	const createuserquery = `
+	CREATE TABLE user (
+		id SERIAL PRIMARY KEY,
+		email VARCHAR(255) UNIQUE NOT NULL,
+		password VARCHAR(255) NOT NULL
+	);
+	`;
+	try { 
+	await client.query(createuserquery);
+	console.log("User table created succesfully");
+	}
+	catch(e) { 
+		console.error("Error occured while creating user table",e);
+	}
+	const createTodoquery = `
+	CREATE TABLE todos (
+		id SERIAL PRIMARY KEY,
+		title TEXT NOT NULL,
+		description TEXT ,
+		user_id INTEGER REFERENCES user(id),
+		done BOOLEAN DEFAULT FALSE
+	);`
+	;
+	try { 
+		await client.query(createTodoquery);
+		console.log("todo created succesfully");
+	}
+	catch(e) { 
+		console.error("Error occured while creating",e);
+	}
 }
 createTable();
+
+
+
+
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,20 @@
 import {Client} from "pg";
 
 export async function getclient() { 
-	const client = new Client({connectionString:`postgresql://postgres:harsh@localhost:5431/postgres`});
-	  try { 
+	try {
+	const client = new Client({connectionString:`psql 'postgres://avnadmin:AVNS_kdZr6Y1rjd7Krl6ql_u@pg-a1248e6-varunsh153-47db.h.aivencloud.com:25254/defaultdb?sslmode=require'`});
+		if(!client) { 
+		console.error("error while creating client");
+	}
+	   
 		await client.connect();
 		console.log("Connection success");
 		return client;
 	}
       catch(e) { 
 	console.error("Error occured while connecting to db",e);
+
 }
 }
+
 
