0f88e3bc02932783304df8bfbea1fb44e2980c59
Varun789-mx <varunsh153@gmail.com>
2025-03-04 17:46:51 +0000

    Add insert-data.ts funciton

--- src/insert-data.ts
+++ src/insert-data.ts
@@ -1,1 +1,13 @@
+import {getclient} from "./utils"
 
+export async function Insertdata() { 
+	const client = await getclient();
+	const insertquery = `INSERT INTO users($1,$2,$3)`;
+	try { 
+	  await client.query(insertquery(1,"Hpandey057@gmail.com","Ninjahattori"));
+          console.log("User added succesfully");
+	}
+	catch(e) { 
+		console.error("Error in adding user to db"+e);
+	}
+}
