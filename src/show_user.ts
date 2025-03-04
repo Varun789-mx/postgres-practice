a1d6224ad61009a85a78819c423e10e79611613d
Varun789-mx <varunsh153@gmail.com>
2025-03-04 18:21:24 +0000

    Fix the show user function by implimenting loop

--- src/show_user.ts
+++ src/show_user.ts
@@ -1,12 +1,22 @@
 import {getclient} from "./utils"
 
 export async function Showusers() { 
-	const client = getclient();
+	const client = await getclient();
 	const showquery = `SELECT * FROM users `;
 	try { 
-		await client.query(showquery);
-		console.log("Here is users table");
+	const result = 	await client.query(showquery);
+	if(result.rows.length === 0) { 
+		console.log("Empty table please add users");
 	}
+	else { 
+		console.log("Here is users table");
+		for(let i = 1;i<result.rows.length;i++) { 
+			console.log(`Id: ${result.rows[i].id}`);
+			console.log(`Email: ${result.rows[i].email}`);
+			console.log(`Password: ${result.rows[i].password}`);
+		}
+	}
+}
 	catch(e) { 
 		console.error("Error in getting users from table");
 	}
