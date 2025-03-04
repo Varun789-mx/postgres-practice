cb6ffa80f0e8f917db1100bd7606d8488c10b2c7
Varun789-mx <varunsh153@gmail.com>
2025-03-04 19:06:44 +0000

    Add test function show.ts

---
+++ src/show.ts
@@ -0,0 +1,17 @@
+   import {getclient} from "./utils";
+  
+   export async function Show() {
+           const client = await getclient();
+           const showquery = `SELECT * FROM users`;
+           try {
+                  console.log("Here is the table");
+                   const res = client.query(showquery);
+                   console.log(res);
+         }
+      catch(e) {
+                  console.error(`Error in ${e}`);
+          }
+          finally {
+              client.end();
+          }
+  }
