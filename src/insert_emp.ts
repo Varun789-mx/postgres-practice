6526bfc8f9be271e9d0af3323c4f99a6784a949c
Varun789-mx <varunsh153@gmail.com>
2025-03-05 18:27:23 +0000

    Added insertemp function

---
+++ src/insert_emp.ts
@@ -0,0 +1,26 @@
+import {getclient} from "./utils"
+
+interface Employee{ 
+	name:string,
+	email:string,
+	mobile:string,
+	salary:number
+}
+
+export async function Insertemp(prop:Employee) { 
+	const client = await getclient();
+	const insertquery = `INSERT INTO employee (name,email,mobile,salary) values ($1,$2,$3,$4) `;
+	const insertprop = [prop.name,prop.email,prop.mobile,prop.salary];
+	try { 
+		await client.query(insertquery,insertprop);
+		console.log("Employee added successfully");
+	}
+	catch(e) { 
+		console.log(`Error ${e}`);
+	}
+	finally{ 
+		client.end();
+	}
+}
+
+Inseremp({name:Harsh,email:"Hpandey057@gmail.com",mobile:"9711018780",salary:100000.00});
