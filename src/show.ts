d9d1b01dc27e723cf945ac4b3a1a5754eafcc5d6
Varun789-mx <varunsh153@gmail.com>
2025-03-05 20:01:26 +0000

    Fix the show function by adding a loop and await

--- src/show.ts
+++ src/show.ts
@@ -1,14 +1,17 @@
-   import {getclient} from "./utils";
+import {getclient} from "./utils";
   
-   export async function Show() {
+export async function Show() {
            const client = await getclient();
            const showquery = `SELECT * FROM users`;
            try {
-                  console.log("Here is the table");
-                   const res = client.query(showquery);
-                   console.log(res);
-         }
-      catch(e) {
+                   console.log("Here is the table");
+                   const res = await client.query(showquery);
+                   for(let i = 0;i<res.rows.length;i++) { 
+			console.log(`Email: ${res.rows[i].email} `);
+			console.log(`Password: ${res.rows[i].password} `);
+		   }
+ }
+      	catch(e) {
                   console.error(`Error in ${e}`);
           }
           finally {
